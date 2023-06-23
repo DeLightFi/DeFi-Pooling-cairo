e866fc86cb926ef6a95348765b1572b8b488f7c9
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-06-24 00:35:53 +0200

    init trade

--- frontend/components/Pool/PoolTrade/index.tsx
+++ frontend/components/Pool/PoolTrade/index.tsx
@@ -1,14 +1,48 @@
 import { useEffect, useState } from "react";
-import { FaChartPie, FaLock } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 import { Container } from "./PoolTradeElements";
 
 
 const PoolTrade = ({ }) => {
+  const [mode, setMode] = useState("deposit");
 
   return (
     <Container>
-
+      <span className="title">Make a trade</span>
+      <div className="trade">
+        {mode === "deposit" ?
+          <div className="btn selected" onClick={() => setMode('deposit')}>
+            <div>
+              <FaLock />
+            </div>
+            <span>Deposit</span>
+          </div>
+          :
+          <div className="btn" onClick={() => setMode('deposit')}>
+            <span className="unselected">Deposit</span>
+          </div>
+        }
+        {mode === "reedem" ?
+          <div className="btn selected" onClick={() => setMode('reedem')}>
+            <span>Reedem</span>
+            <div>
+              <FaLock />
+            </div>
+          </div>
+          :
+          <div className="btn" onClick={() => setMode('reedem')}>
+            <span className="unselected">Reedem</span>
+          </div>
+        }
+      </div>
+      <div className="value">
+        <span>How much</span>
+        <input type="text" />
+      </div>
+      <div className="submit">
+        <button>Submit</button>
+      </div>
     </Container>
   );
 };
