1c50f362ba56d40a4832c256cbd85e173d117bf0
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-06-23 21:36:49 +0200

    add argent connection

--- frontend/components/Layout/Navbar/index.tsx
+++ frontend/components/Layout/Navbar/index.tsx
@@ -1,15 +1,47 @@
+import { connect, disconnect } from '@argent/get-starknet'
+
 import { FaWindowMaximize } from "react-icons/fa";
 import { Container } from "./NavbarElements";
 
 
-const Navbar = ({ }) => {
+
+const Navbar = ({ connection, setConnection, setProvider, setAddress }) => {
+    const connectToStarknet = async () => {
+        const connection = await connect({
+            modalWalletAppearance: "all"
+        });
+        console.log(connection)
+        if (connection && connection.isConnected) {
+            console.log("there", connection)
+            setConnection(connection);
+            setProvider(connection.account);
+            setAddress(connection.selectedAddress);
+        }
+    };
+
+    const disconnectWallet = async () => {
+        await disconnect();
+        setConnection(undefined);
+        setProvider(undefined);
+        setAddress('');
+    }
+
+    console.log(connection === null || connection === undefined);
+
     return (
         <Container>
             <span>mirror</span>
-            <div>
-                <FaWindowMaximize />
-                Connect
-            </div>
+            {connection === null || connection === undefined ?
+                <div onClick={async () => await connectToStarknet()}>
+                    <FaWindowMaximize />
+                    Connect
+                </div>
+                :
+                <div onClick={async () => await disconnectWallet()}>
+                    <FaWindowMaximize />
+                    Disconnect
+                </div>
+            }
         </Container>
     );
 };
