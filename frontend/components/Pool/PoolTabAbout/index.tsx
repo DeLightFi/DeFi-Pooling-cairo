66e57ac1638ef2db870ba9c7f4a0215628985dac
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-06-25 14:19:00 +0200

    init about tab

--- frontend/components/Pool/PoolTabAbout/index.tsx
+++ frontend/components/Pool/PoolTabAbout/index.tsx
@@ -4,7 +4,26 @@ import { Container } from "./PoolTabAboutElements";
 const PoolTabAbout = ({ }) => {
   return (
     <Container>
-      <span>TODO</span>
+      <h3>This is mirror</h3>
+      <span>
+        Joining, leaving, and re-balancing a L1 Pool is expensive, often prohibitively so. Layer 2 (L2) transaction pooling and matching enables sharing the transaction costs across a larger group. It has taken years and years to build a resilient DeFi ecosystem on Ethereum, with deep liquidity and more complex strategies.
+        This POC aims to provide an innovative solution in this transitioning phase while we have most of the liquidity and usability still on L1.
+      </span>
+      <h3>Thanks to third-parties protocol</h3>
+      <div className="partners">
+        <div>
+          <img src="https://www.pragmaoracle.com/pragma-logo.svg" alt="pragma" />
+          <span>Organizing the first starknet - cairo 1 hackathon.</span>
+        </div>
+        <div>
+          <img src="https://www.herodotus.dev/logo-full-blue-white.svg" alt="herodotus" />
+          <span>Providing interaction between L2 and L1.</span>
+        </div>
+        <div>
+          <img src="https://www.pragmaoracle.com/assets/ecosystem/argent.png" alt="argent" />
+          <span>Offering web wallet solution for developpers.</span>
+        </div>
+      </div>
     </Container>
   );
 };
