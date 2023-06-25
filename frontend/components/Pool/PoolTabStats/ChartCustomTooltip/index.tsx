06e5b72192a44b2eff217a6682ce651c2d27dd25
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-06-25 20:18:28 +0200

    improve tooltip

--- frontend/components/Pool/PoolTabStats/ChartCustomTooltip/index.tsx
+++ frontend/components/Pool/PoolTabStats/ChartCustomTooltip/index.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import moment from "moment";
 import { Container } from "./ChartCustomTooltipElements";
 
+import { formatNumber } from "../../../../utils";
+
+
 const CustomTooltip = ({ payload }) => {
   if (payload && payload.length) {
     const selected_data = payload[0].payload;
 
-    console.log(selected_data)
-
-
     return (
       <Container>
         <span>{moment(new Date(+`${selected_data.timestamp}000`)).format("DD MMMM YYYY h:mm")}</span>
-        <span>{(+selected_data.apy).toFixed(2)}</span>
+        <span>{formatNumber(+selected_data.apy)}</span>
       </Container>
     );
   }
-
   return null;
 };
-
 export default CustomTooltip;
